a19654569e3d65f630dc91cb18b2d76f61c87447
cheikh sadbouh <cheikhsoftdev@gmail.com>
2020-12-02 22:00:35 +0800

    feat: create user-book-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LibraryBookListComponent } from './components/library-book-list/library-book-list.component';
+import { UserBookListComponent } from './components/user-book-list/user-book-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LibraryBookListComponent
+    LibraryBookListComponent,
+    UserBookListComponent
   ],
   imports: [
     BrowserModule,
